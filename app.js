8ca74c712b9d2f0e42a91e27e318d06171cc3be8
chenpenfei <812651219@qq.com>
2018-10-11 21:26:17 +0800

    部署

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 require('./database/config');
+
 var MongoStore = require('connect-mongo')(session);
 
 
